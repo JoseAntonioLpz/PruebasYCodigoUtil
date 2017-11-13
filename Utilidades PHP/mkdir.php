62dd587074b627fb6298e079828084dfca05890b
Jose Antonio Lopez Lopez <joseantoniolopez241197@gmail.com>
2017-11-13 19:38:58 +0100

    13112017

---
+++ Utilidades PHP/mkdir.php
@@ -0,0 +1,12 @@
+<?php
+//Fuente php.net
+function mkdir_r($dirName, $rights=0777){
+    $dirs = explode('/', $dirName);
+    $dir='';
+    foreach ($dirs as $part) {
+        $dir.=$part.'/';
+        if (!is_dir($dir) && strlen($dir)>0)
+            mkdir($dir, $rights);
+    }
+}
+?>

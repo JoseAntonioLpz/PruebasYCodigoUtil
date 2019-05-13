70365226e0a6bf782337262ea1b09643fcc2fab0
Jose Antonio Lopez Lopez <joseantoniolopez241197@gmail.com>
2019-05-13 16:59:23 +0200

    Create Ramdonizer.php

---
+++ Utilidades PHP/Ramdonizer.php
@@ -0,0 +1,32 @@
+<?php
+
+Class Ramdonizer{
+    
+    private $exit;
+    private $char;
+    private $lenght;
+
+    function __construct($lenght = 8, $char = 'abcdefghijklmnopqrstuvwxyz1234567890'){
+        $this->char = $char;
+        $this->lenght = $lenght;
+    }
+
+    private function generateRandomStr(){
+    	$this->exit = substr(str_shuffle(str_repeat($this->char, 5)), 0, $this->lenght);
+
+    	return $this->exit;
+    }
+
+    function get(){
+    	return $this->generateRandomStr();
+    }
+
+    function setChar($char){
+    	$this->char = $char;
+    }
+
+    function setLenght($lenght){
+    	$this->lenght = $lenght;
+    }
+
+}

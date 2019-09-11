8eaca7a884d9f7027d9af5ddca1987fb88ec47fc
Jose Antonio Lopez Lopez <joseantoniolopez241197@gmail.com>
2019-09-11 12:31:48 +0200

    add MP3File example

---
+++ Utilidades PHP/Examples/mp3duration/index.php
@@ -0,0 +1,11 @@
+<?php
+    require('MP3File.php');
+
+    // Aqui deberias indicar el archivo MP3 que quieras usar
+    $mp3file = new MP3File('audio.mp3');
+    $duration1 = $mp3file->getDurationEstimate();
+    $duration2 = $mp3file->getDuration();
+
+    echo "Duración: $duration1 segundos"."<br/>";
+    echo "Estimado: $duration2 segundos"."<br/>";
+    echo "Estimado formateado: ".MP3File::formatTime($duration2)."<br/>";
